a4dda6319d07f71f4250636e8ca425fbf1ca1711
masked <37945842+maskeddd@users.noreply.github.com>
2025-12-24 15:03:05 +1000

    Update devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   languages = {
@@ -7,4 +7,31 @@
       channel = "stable";
     };
   };
+
+  packages = lib.optionals pkgs.stdenv.isLinux (
+    with pkgs;
+    [
+      libxkbcommon
+      libGL
+      wayland
+      xorg.libXcursor
+      xorg.libXrandr
+      xorg.libXi
+      xorg.libX11
+    ]
+  );
+
+  # Only define LD_LIBRARY_PATH on Linux
+  env.LD_LIBRARY_PATH = lib.mkIf pkgs.stdenv.isLinux (
+    lib.makeLibraryPath [
+      pkgs.libxkbcommon
+      pkgs.libGL
+      pkgs.wayland
+      pkgs.xorg.libXcursor
+      pkgs.xorg.libXrandr
+      pkgs.xorg.libXi
+      pkgs.xorg.libX11
+    ]
+  );
+
 }
